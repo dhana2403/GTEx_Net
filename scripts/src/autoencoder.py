88c094144c82f75119bd5f8e11469a1130c68c69
Dhanalakshmi Jothi <91247537+dhana2403@users.noreply.github.com>
2025-06-21 12:04:49 +0200

    Update autoencoder.py

--- scripts/src/autoencoder.py
+++ scripts/src/autoencoder.py
@@ -6,17 +6,17 @@ from sklearn.preprocessing import StandardScaler
 import numpy as np
 
 class Autoencoder(nn.Module):
-    def __init__(self, input_dim, latent_dim=10):
+    def __init__(self, input_dim, latent_dim=3):
         super(Autoencoder, self).__init__()
         self.encoder = nn.Sequential(
-            nn.Linear(input_dim, 64),
+            nn.Linear(input_dim, 8),
             nn.ReLU(),
-            nn.Linear(64, latent_dim)
+            nn.Linear(8, latent_dim)
         )
         self.decoder = nn.Sequential(
-            nn.Linear(latent_dim, 64),
+            nn.Linear(latent_dim, 8),
             nn.ReLU(),
-            nn.Linear(64, input_dim)
+            nn.Linear(8, input_dim)
         )
     
     def forward(self, x):
